097afc6f10c913d95116ae4e5f85cd17d28fb359
Hamza Hmem <hhmem@tkyr.net>
2023-12-10 08:50:47 +0100

    feat: upgrade the react native version to 0.63.5

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import android.app.Application;
-
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
